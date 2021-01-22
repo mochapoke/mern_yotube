fd418fd08856041dfe45453e2c89f3b411e142a9
mocha <syda07@gmail.com>
2021-01-23 08:25:01 +0900

    ✨ LandingPage에 DB정보 뿌리기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,75 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { Card, Icon, Avatar, Col, Typography, Row } from 'antd';
+import axios from 'axios';
+import moment from 'moment';
+
+const { Title } = Typography;
+const { Meta } = Card;
 
 function LandingPage() {
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/video/getVideos').then((res) => {
+      if (res.data.success) {
+        setVideo(res.data.videos);
+        console.log(res.data.videos);
+      } else {
+        alert('비디오 가져오기 실패');
+      }
+    });
+  }, []);
+
+  const rederCards = video.map((video, index) => {
+    let hours = Math.floor(video.duration / 60 / 60);
+
+    let minutes = Math.floor(video.duration / 60);
+    let seconds = Math.floor(video.duration - minutes * 60);
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+      <Col lg={6} md={8} xs={24}>
+        <a href={`/video/post/${video._id}`}>
+          <div style={{ position: 'relative' }}>
+            <img
+              style={{ width: '100%' }}
+              src={`http://localhost:5000/${video.thumbnail}`}
+              alt='thumbnail'
+            />
+            <div className='duration'>
+              <span>
+                {hours >= 1 ? (
+                  <span>
+                    {hours}: {minutes} : {seconds}
+                  </span>
+                ) : (
+                  <span>
+                    {minutes} : {seconds}
+                  </span>
+                )}
+              </span>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+          </div>
+        </a>
+        <br />
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+          description=''
+        />
+        <span>{video.writer.name}</span>
+        <span style={{ marginLeft: '3rem' }}>{video.views} views</span> -
+        <span>{moment(video.createAt).format('MMM Do YY')} </span>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: '85%', margin: '3rem auto' }}>
+      <Title levle={2}> Recomended</Title>
+      <hr />
+      <Row gutter={[32, 16]}>{rederCards}</Row>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
